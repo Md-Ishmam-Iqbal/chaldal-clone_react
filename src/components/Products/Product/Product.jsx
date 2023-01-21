13c6e1dfdd2f7f2d187ce22777c5d848aa44ed4f
Md-Ishmam-Iqbal <iqbal.ishmam@gmail.com>
2023-01-21 21:31:35 +0600

    drafted product styling, importing product data

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@mui/material'
+import { AddShoppingCart } from '@mui/icons-material'
+
+// import styles
+
+const Product = ({ product }) => {
+  return (
+    <Card>
+        <CardMedia image='' title={product.name}/>
+        <CardContent>
+            <div>
+                <Typography variant="h5" gutterBottom>
+                    {product.name}
+                </Typography>
+                <Typography variant="h5" gutterBottom>
+                    {product.price}
+                </Typography>
+            </div>
+            <Typography variant="body2" color="textSecondary">
+                {product.description}
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+            <IconButton aria-label="Add to cart">
+                <AddShoppingCart />
+            </IconButton>
+        </CardActions>
+    </Card>
+  )
+}
+
+export default Product
